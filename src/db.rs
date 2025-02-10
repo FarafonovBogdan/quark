9ca30eb45a9cc299c8ce3f30aebcc12a12afcea6
FarafonovBogdan <bogdanf.dev@gmail.com>
2025-02-10 10:27:31 +0200

    add impl for db

--- src/db.rs
+++ src/db.rs
@@ -2,6 +2,43 @@ use sled::Db;
 use std::sync::Arc;
 
 #[derive(Clone)]
-pub struct Database {}
+pub struct Database {
+    db: Arc<Db>,
+    read_only: bool,
+}
 
-impl Database {}
+impl Database {
+    pub fn new(path: &str, read_only: bool) -> sled::Result<Self> {
+        let db = sled::open(path)?;
+        Ok(Self {
+            db: Arc::new(db),
+            read_only,
+        })
+    }
+
+    pub fn set(&self, key: &str, value: &str) -> sled::Result<()> {
+        if self.read_only {
+            return Err(sled::Error::Unsupported("Read-only mode".into()));
+        }
+        self.db.insert(key, value.as_bytes())?;
+        self.db.flush()?;
+        Ok(())
+    }
+
+    pub fn get(&self, key: &str) -> Option<String> {
+        self.db
+            .get(key)
+            .ok()
+            .flatten()
+            .map(|v| String::from_utf8(v.to_vec()).unwrap())
+    }
+
+    pub fn delete(&self, key: &str) -> sled::Result<()> {
+        if self.read_only {
+            return Err(sled::Error::Unsupported("Read-only mode".into()));
+        }
+        self.db.remove(key)?;
+        self.db.flush()?;
+        Ok(())
+    }
+}
